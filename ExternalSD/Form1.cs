f7eda72efb35ed11f1e9e979074b7a846c36002a
Tyler Cottington <tcottington1995@gmail.com>
2019-11-26 12:23:57 -0500

    Start Of External Screen Capture
    Current design of external system. Is able to print screen by way of windows form.

---
+++ ExternalSD/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace ExternalSD
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            printscreen.Save(@"C:\Users\Tyler\Pictures\ExternalTest\printscreen.jpg", ImageFormat.Jpeg);
+        }
+    }
+}
